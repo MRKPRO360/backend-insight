14d4e61d139141320c7b493f75ee21fda5726fc4
Rezaul <mdrezaulkarrim@gmail.com>
2024-12-18 23:47:07 +0600

    added not found middleware and blog router in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import cors from 'cors';
-
 import express, { Application } from 'express';
+import router from './app/routes';
+import notFound from './app/middlewares/not-found';
 
 const app: Application = express();
 
 // BODY PARSER
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:3000'] }));
 
+// APPLICATION ROUTERS
+app.use('/api', router);
+
+// GENERIC RESPONSE
 app.use('/', (req, res) => {
   res.send('HELLO WORLD 👋');
 });
+
+//NOT-FOUND
+app.use(notFound);
 
 export default app;
--- src/app/modules/blog/blog.routes.ts
+++ src/app/modules/blog/blog.routes.ts
@@ -1,16 +1,24 @@
 import { Router } from 'express';
 import BlogControllers from './blog.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import BlogValidations from './blog.validation';
 
 const router = Router();
 
 router
   .route('/blogs')
-  .post(BlogControllers.createABlog)
+  .post(
+    validateRequest(BlogValidations.createBlogValidationSchema),
+    BlogControllers.createABlog,
+  )
   .get(BlogControllers.getAllBlogs);
 
 router
   .route('/blogs/:id')
-  .patch(BlogControllers.updateABlog)
+  .patch(
+    validateRequest(BlogValidations.updateBlogValidationSchema),
+    BlogControllers.updateABlog,
+  )
   .delete(BlogControllers.deleteABlog);
 
 export default router;
